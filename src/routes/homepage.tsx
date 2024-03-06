b1d8e8c30764d02220bb879cfe860e4ca024349a
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-03-06 14:20:17 +0100

    Making Homepage and ProductList for  responsive mobile

--- src/routes/homepage.tsx
+++ src/routes/homepage.tsx
@@ -9,27 +9,48 @@ import {
 export default function Homepage() {
     return (
         <div id='homepage'>
-            <div className='px-2 flex'>
+            <div className='px-3 flex'>
                 
                 <div id='accordion' className='flex-intial w-32 '>
                 <Accordion type="single" collapsible>
 
-                <AccordionItem value="item-1">
+                <AccordionItem value="item-1" className='py-2'>
                     <AccordionTrigger>Clothes</AccordionTrigger>
                     <AccordionContent>
                     Yes. It adheres to the WAI-ARIA design pattern.
                     </AccordionContent>
                 </AccordionItem>
 
-                <AccordionItem value="item-2">
+                <AccordionItem value="item-2" className='py-2'>
                     <AccordionTrigger>Bags</AccordionTrigger>
                     <AccordionContent>
                     Yes. It adheres to the WAI-ARIA design pattern.
                     </AccordionContent>
                 </AccordionItem>
 
-                <AccordionItem value="item-3">
+                <AccordionItem value="item-3" className='py-2'>
                     <AccordionTrigger>Shoes</AccordionTrigger>
+                    <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-4" className='py-2'>
+                    <AccordionTrigger>Hair</AccordionTrigger>
+                    <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-5" className='py-2'>
+                    <AccordionTrigger>MakeUp</AccordionTrigger>
+                    <AccordionContent>
+                    Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+
+                <AccordionItem value="item-6" className='py-2'>
+                    <AccordionTrigger>Accessories</AccordionTrigger>
                     <AccordionContent>
                     Yes. It adheres to the WAI-ARIA design pattern.
                     </AccordionContent>
--- src/routes/productList.tsx
+++ src/routes/productList.tsx
@@ -3,9 +3,9 @@ export default function ProductList() {
         <div id="productlist">
 
 <div className='container flex-auto py-4'>
-                    <h1 className='text-lg font-semibold'>Welcome to FA Online Shop</h1>
+                    <h1 className='text-base font-semibold'>Welcome to FA Online Shop</h1>
 
-                    <div className="grid grid-cols-3 grid-flow-row gap-4 pt-3">
+                    <div className="grid grid-cols-1 grid-flow-row gap-3 pt-3">
                         <div className='productImg'>
                         <img src="https://placehold.co/400" alt="1" />
                         <div className='productText'>
@@ -33,59 +33,6 @@ export default function ProductList() {
                         </div>
                         </div>
 
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="4" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
-
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="5" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
-
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="6" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
-
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="7" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
-
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="8" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
-
-                        <div className='productImg'>
-                        <img src="https://placehold.co/400" alt="9" />
-                        <div className='productText'>
-                            <h6>FA</h6>
-                            <p>Lorem ipsum dolor...</p>
-                            <p>200 sek</p>
-                        </div>
-                        </div>
                     </div>
                 </div>
         </div>
