2526f3f572ada73bb41fb685782f30fb97be50bc
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-03-11 19:41:02 +0100

    Creating types.ts...
    - ..., and hooks.ts to be ble to re-use the types and hooks in multiple components in the application.
    
    - navigation button made dynamic

---
+++ src/routes/hooks.ts
@@ -0,0 +1,38 @@
+import { useQuery } from '@tanstack/react-query'
+import { useParams } from 'react-router-dom';
+
+export function useGetProducts(){
+    const { data: products, error, isLoading } = useQuery({
+        queryFn: () =>
+        fetch('https://fakestoreapi.com/products?limit=10').then(
+            (res) => res.json()
+        ),
+        queryKey: ['products'],
+    });
+
+return {
+    products,
+    error,
+    isLoading
+}
+}
+
+export function useGetProduct() {
+    const {productId} = useParams();
+
+    const { data: singleProduct, error, isLoading } = useQuery({
+        queryFn: () =>
+        fetch(`https://fakestoreapi.com/products/${productId}`).then(
+            (res) => res.json()
+        ),
+        queryKey: ['productId', productId],
+    });
+
+    return {
+        singleProduct,
+        error,
+        isLoading
+    }
+}
+
+
--- src/routes/productList.tsx
+++ src/routes/productList.tsx
@@ -1,67 +1,49 @@
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { useQuery } from '@tanstack/react-query'
-import { useNavigate } from 'react-router-dom'
-
-//interface always outside of the component here ex Homepage
-interface Product{
-    id: number,
-    title: string,
-    price: number,
-    description: string,
-    category: string,
-    image?: string,
-    rating: {
-        rate: number,
-        count: number,
-    }
-};
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import { useGetProducts } from './hooks';
+import { useNavigate } from 'react-router-dom';
+import Product from './types';
 
 export default function ProductList() {
-        const { data: products, error, isLoading } = useQuery({
-            queryFn: () =>
-            fetch('https://fakestoreapi.com/products?limit=10').then(
-                (res) => res.json()
-            ),
-            queryKey: ['products'],
-        });
+
+    const { products, error, isLoading } = useGetProducts();
 
         const navigate = useNavigate();
 
-if(error) {
-    return <div>Not able to fetch any products, try again later!</div>
-}
+        if(error) {
+            return <div>{error.message}</div>
+        }
 
-if(isLoading) {
-    return <p>Loading...</p>
-}
+        if(isLoading) {
+            return <p>Loading...</p>
+        }
 
-    return (
-        <div id="productlist">
-            <div className='container flex-auto'>
-                <div className="grid grid-cols-1 grid-flow-row gap-4">
-                    <h1 className='text-base font-semibold py-4'>Welcome to FA Online Shop</h1>
-             {products.map((product: Product) => (
+            return (
+                <div id="productlist">
+                    <div className='container flex-auto'>
+                        <div className="grid grid-cols-1 grid-flow-row gap-4">
+                            <h1 className='text-base font-semibold py-4'>Welcome to FA Online Shop</h1>
+                    {products.map((product: Product) => (
 
-                <Card>
-                    <CardHeader>
-                        <CardTitle>{product.title}</CardTitle>
-                        <img src={product.image} alt="productImage" />
-                        <CardDescription>{product.description}</CardDescription>
-                    </CardHeader>
-                    <CardContent>
-                        <p>{product.price} sek</p>
-                        <p>Rate: {product.rating.rate}</p>
-                        <p className='py-4'>Reviews: {product.rating.count}</p>
-                       <Button>
-                       <div className='goToProduct' onClick={() => navigate('/product/:productId')}>Go To Product</div>
-                       </Button>
-                        
-                    </CardContent>
-                </Card>
-        ))}
-            </div>
-            </div>
-        </div>
-        );
+                        <Card key={product.id}>
+                            <CardHeader>
+                                <CardTitle>{product.title}</CardTitle>
+                                <img src={product.image} alt="productImage" />
+                                <CardDescription>{product.description}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p>{product.price} sek</p>
+                                <p>Rate: {product.rating.rate}</p>
+                                <p className='py-4'>Reviews: {product.rating.count}</p>
+                            <Button>
+                            <div className='goToProduct' onClick={() => navigate(`/product/${product.id}`)}>Go To Product</div>
+                            </Button>
+                                
+                            </CardContent>
+                        </Card>
+                ))}
+                    </div>
+                    </div>
+                </div>
+                );
 }
