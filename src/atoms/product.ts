f6d4d944a843685e1bd4d4784dc706299b8fe61f
Farah Alhaddad <120371923+Farahalh@users.noreply.github.com>
2024-03-22 11:59:09 +0100

    changing from atom to atomWithStorage for persisted storage on har reload

--- src/atoms/product.ts
+++ src/atoms/product.ts
@@ -1,13 +1,13 @@
-import { atom } from 'jotai'
-//future use atomWithStorage 
+import { atomWithStorage } from "jotai/utils";
+//future use atomWithStorage
 
 export interface Product {
-id: number,
-image?: string,
-title: string,
-price: number,
-quantity: number,
+  id: number;
+  image?: string;
+  title: string;
+  price: number;
+  quantity: number;
 }
 
 //future use atomWithStorage
-export const productAtom = atom<Product[]>([]);
+export const productAtom = atomWithStorage<Product[]>('productAtom', []);
